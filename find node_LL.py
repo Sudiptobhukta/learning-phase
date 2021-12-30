248c58cc76d8fef3f35fa3bd9cc40c50b33fbe1d
Sudiptobhukta <sudipto11bhukta@gmail.com>
2021-12-30 21:24:48 +0530

    fid the node is added

---
+++ find node_LL.py
@@ -0,0 +1,42 @@
+class node:
+    def __init__(self,data) -> None:
+        self.data= data
+        self.next = None
+
+def insertinglist():
+    list=[int(i) for i in input().split(" ")]
+    head = None
+    tail = None
+    for inputdata in list:
+        if inputdata == -1:
+            break
+        newnode= node(inputdata)
+        if head is None:
+            head = newnode
+            tail = newnode
+        else:
+            tail.next = newnode
+            tail = newnode
+    return head
+
+def findnode(head,n):
+    if head == n:
+        return 0
+    if head is None:
+        return -1
+    find = findnode(head.next,n)
+    if find== -1:
+        return -1
+    else:
+        return find+1
+
+def printll(x):
+    while x is not None:
+        print(str(x.data) +'-:>',end=(" "))
+        x=x.next
+    print(None)
+    return
+
+head= insertinglist()
+printll(head)
+print(findnode(head,4))
