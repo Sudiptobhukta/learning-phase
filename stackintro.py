72a5a13b72e5be83611d971c34f625b2fff6de8b
Sudiptobhukta <sudipto11bhukta@gmail.com>
2022-01-07 18:35:34 +0530

    stackchecked changes from stack

--- stackintro.py
+++ stackintro.py
@@ -1,26 +1,27 @@
-class stack:
-    def __init__(self) -> None:
-        self.data= []
-    
+class stackcheck:
+    def __init__(self):
+        self.data = []
+
     def push(self,item):
         self.data.append(item)
 
     def pop(self):
         if self.isEmpty():
-            print('Hey stack is empty')
+            print("Hey this is end of stack")
             return
+
         return self.data.pop()
 
-    def top(self):
-        if self.isEmpty():
-            print(" Hey stack is Empty")
-            return 
-        return self.data[len(self.data)-1]
-
     def isEmpty(self):
-        if self.size()==0:
-            return ("hey it empty")
-    
+        return self.size() == 0
+
     def size(self):
         return len(self.data)
+    
+    def top(self):
+        if self.isEmpty():
+            print("Hey this stack is empty")
+
+        return self.data[len(self.data)-1]
+
         
